ac893d22e4b9e7e0c808c697c955f05bcdf6c87d
haitruongdfi <hai.truong@datafluct.com>
2023-06-28 11:08:02 +0700

    update

---
+++ components/AppHeader.vue
@@ -0,0 +1,3 @@
+<template>
+  <section><h1>This is Header</h1></section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+const runtimeConfig = useRuntimeConfig(); // load everything in nuxt.config.ts
+const appConfig = useAppConfig(); // load everything in app.config.ts
+console.log(runtimeConfig.apiSecret);
+</script>
+<template>
+  <Title>Nuxt 3 Lab</Title>
+  <Meta name="description" content="Description for page" />
+
+  <h1>{{ appConfig.title }}</h1>
+  <p>{{ appConfig.theme.colors.primary }}</p>
+  <p>{{ runtimeConfig.public.apiBase }}</p>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <AppAlert> This is an auto-imported component </AppAlert>
+  </div>
+</template>
